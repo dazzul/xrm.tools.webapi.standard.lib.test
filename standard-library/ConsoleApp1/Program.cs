ca759547cbf10c2978cb95b82419092e85292970
dazzul <uchihajax@gmail.com>
2018-11-22 15:26:48 +0000

    Add files via upload

---
+++ standard-library/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using standard_library;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new ExtendedComponent();
+            var task = s.GoDo();
+            task.Wait();
+        }
+    }
+
+    public class ExtendedComponent : SomeComponent
+    {
+        public async Task GoDoSpecific()
+        {
+            var api = CreateApi();
+            await api.Delete(string.Empty, Guid.Empty);
+        }
+    }
+}
---
+++ standard-library/standard-library/SomeComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xrm.Tools.WebAPI;
+
+namespace standard_library
+{
+    public class SomeComponent
+    {
+        public async Task GoDo()
+        {
+            var api = CreateApi(); 
+            await api.Create(string.Empty, null);
+        }
+
+        protected CRMWebAPI CreateApi()
+        {
+            return new CRMWebAPI(string.Empty, new NetworkCredential());
+        }
+    }
+}
